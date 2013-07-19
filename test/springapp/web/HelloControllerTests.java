9476a907a12b345772e97f370cf5bd6b72be6fb1
unknown <doyu@L-SHC-00426427.corp.ebay.com>
2013-07-19 15:08:18 +0800

    part2: Developing and Configuring the Views and the Controller

--- test/springapp/web/HelloControllerTests.java
+++ test/springapp/web/HelloControllerTests.java
@@ -8,8 +8,11 @@ import junit.framework.TestCase;
 public class HelloControllerTests extends TestCase {
 
     public void testHandleRequestView() throws Exception{		
-        HelloController controller = new HelloController();
-        ModelAndView modelAndView = controller.handleRequest(null, null);		
-        assertEquals("hello.jsp", modelAndView.getViewName());
+    	 HelloController controller = new HelloController();
+         ModelAndView modelAndView = controller.handleRequest(null, null);
+         assertEquals("hello", modelAndView.getViewName());
+         assertNotNull(modelAndView.getModel());
+         String nowValue = (String) modelAndView.getModel().get("now");
+         assertNotNull(nowValue);
     }
 }
